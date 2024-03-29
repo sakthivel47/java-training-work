358258f09c0810b58fd4e833dc69a67ffca2a057
sakthivel47 <p.sakthi4702@gmail.com>
2024-03-29 09:58:12 +0530

    update assignment file

--- java_assignments/file_io/ListFiles.java
+++ java_assignments/file_io/ListFiles.java
@@ -6,34 +6,40 @@ import java.util.List;
 
 public class ListFiles {
 	public static void main(String[] args) {
+		
+		try {
+			List<String> files_list = new ArrayList<String>();
+			boolean check = false;
+			File[] files = new File(args[0]).listFiles();
+			
+			if(files == null) {
+				System.out.println("No such directory present!");
+			} else {
 
-		List<String> files_list = new ArrayList<String>();
-		boolean check = false;
-		File[] files = new File(args[0]).listFiles();
-		if(files == null) {
-			System.out.println("No such directory present!");
-		} else {
-
-			for(File file : files) {
-				if(file.isFile()) {
-					files_list.add(file.getName());
-					check = true;
+				for(File file : files) {
+					if(file.isFile()) {
+						files_list.add(file.getName());
+						check = true;
+					}
+				}
+				if(!check) {
+					System.out.println("No files in this directory");
 				}
 			}
-			if(!check) {
-				System.out.println("No files in this directory");
+			boolean checkNoFile = false;
+			for(String filename: files_list) {
+				String ext = filename.substring(filename.indexOf('.')+1, filename.length());
+				if(ext.equals(args[1])) {
+					System.out.println(filename);
+					checkNoFile = true;
+				}
 			}
-		}
-		boolean checkNoFile = false;
-		for(String filename: files_list) {
-			String ext = filename.substring(filename.indexOf('.')+1, filename.length());
-			if(ext.equals(args[1])) {
-				System.out.println(filename);
-				checkNoFile = true;
+			if(!checkNoFile) {
+				System.out.println("No files of that given extension");
 			}
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("No arguments given as input\nArguments usage: path extension");
 		}
-		if(!checkNoFile) {
-			System.out.println("No files of that given extension");
-		}
+		
 	}
 }
